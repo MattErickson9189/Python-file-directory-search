cb34f2d978b86363f6435401d19fab87ebb2f5e0
Matt Erickson <matterickson9819@gmail.com>
2018-09-03 11:33:40 -0500

    Added PDF support and cleaned code

--- RegSearch.py
+++ RegSearch.py
@@ -2,8 +2,13 @@
 # this program searches all the files in a specified directory and picks out the specified pattern
 # Usage: RegSearch <absolute directory path> <Pattern>
 
-import os, sys, pyperclip, re, PyPDF2, docx2txt
+import os, sys, pyperclip, re, docx2txt
 from unipath import Path
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 
 
 matches = []
@@ -40,7 +45,37 @@ elif(sys.argv[2] == 'email'):
 elif(sys.argv[2] == 'phone'):
     Search = regexPhone
 
+#function to convert pdf to text
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos=set()
 
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    pyperclip.copy(text)
+
+#function to scan text against regSearches
+def regSearch(text):
+    for groups in Search.findall(text):
+        phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+        matches.append(phoneNum)
+    for groups in Search.findall(text):
+        matches.append(groups[0])
 
 # creates the file inside the directory to be walked
 
@@ -53,23 +88,18 @@ for(dirpath, dirnames, filenames) in os.walk(path):
             file = open(os.path.join(path,dirpath,filename), 'r').read()
             pyperclip.copy(file)
             # scans text files for the regex search
-            for groups in Search.findall(pyperclip.paste()):
-                phoneNum = '-'.join([groups[1],groups[3],groups[5]])
-                matches.append(phoneNum)
-            for groups in Search.findall(pyperclip.paste()):
-                matches.append(groups[0])
-        if filename.endswith('.pdf') and  filename.isEncryptec() == False: # looks for pdf files
-            print("Hello")
+            regSearch(pyperclip.paste())
+
+        if filename.endswith('.pdf'): # looks for pdf files
+            convert_pdf_to_txt(os.path.join(path,dirpath,filename))
+            # scans text for regex search
+            regSearch(pyperclip.paste())
 
         if filename.endswith('.docx'): # looks for docx files
             text = docx2txt.process(os.path.join(path,dirpath,filename))
             pyperclip.copy(text)
                 #scans text for regex search
-            for groups in Search.findall(pyperclip.paste()):
-                phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-                matches.append(phoneNum)
-            for groups in Search.findall(pyperclip.paste()):
-                matches.append(groups[0])
+            regSearch(pyperclip.paste())
 
 # copy results to the clipboard and writes to the result.txt file
 
